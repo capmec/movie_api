dc513fbca3e41dfe9cc1d56d817c6a9c92a5c199
capmec <capmec@gmail.com>
2024-05-05 15:49:48 +0200

    description add to the movies model

--- models.js
+++ models.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt')
 let movieSchema = mongoose.Schema({
 	title: { type: String, required: true },
 	year: { type: String, required: true },
+	description: { type: String, required: true },
 	genre: { name: String, description: String },
 	director: { name: String, bio: String, birth: String, death: String },
 	actors: [String],
