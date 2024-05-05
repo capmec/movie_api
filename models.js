7fc4617520f194b6188f637e5ad4d334e35dca47
capmec <capmec@gmail.com>
2024-05-05 18:41:53 +0200

    models genre updated

--- models.js
+++ models.js
@@ -5,7 +5,7 @@ let movieSchema = mongoose.Schema({
 	title: { type: String, required: true },
 	year: { type: String, required: true },
 	description: { type: String, required: true },
-	genre: { type: String, required: true },
+	genre: [String],
 	director: { name: String, bio: String, birth: String, death: String },
 	actors: [String],
 	image: String,
