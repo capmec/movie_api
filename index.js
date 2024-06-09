8c3d765c1ceb67dc8ce17e0ea39de7bd23cb5271
capmec <capmec@gmail.com>
2024-06-09 23:08:16 +0200

    cors config

--- index.js
+++ index.js
@@ -19,15 +19,23 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = [
+	'http://localhost:8080', 
+	'http://testsite.com', 
+	'http://localhost:1234', 
+	'https://my-awesome-site123.netlify.app'
+];
 
-// const corsOptions = {
-// 	// origin: 'http://localhost:1234', // Replace with your frontend URL
-// 	origin: 'https://movie-api-o5p9.onrender.com/',
-// 	methods: ['GET', 'POST', 'PUT', 'DELETE'],
-// 	allowedHeaders: ['Content-Type', 'Authorization'],
-// };
-// app.use(cors(corsOptions));
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth/auth.js')(app);
 const passport = require('passport');
