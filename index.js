981ce8263ea994ee971242b53358021bab23a4e4
capmec <capmec@gmail.com>
2024-07-28 11:27:25 +0200

    mongoose connect online

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const express = require('express'),
 
 const dotenv = require('dotenv');
 dotenv.config();
-//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 //connect LOCAL database
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true,});
+//mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true,});
 
 const app = express();
 app.use(express.json());
